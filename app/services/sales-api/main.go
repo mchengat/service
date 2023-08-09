88f0e1afdf8e7972c8b72894d750aa9221c90693
Mansoor C <mchengat@Mansoors-MacBook-Pro.local>
2023-08-09 07:53:03 +0530

    initial commit

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/ardanlabs/conf/v3"
+	"github.com/mchengat/service/business/web/v1/debug"
+	"github.com/mchengat/service/foundation/logger"
+	"go.uber.org/zap"
+)
+
+var build = "develop"
+
+func main() {
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", nil)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// ---------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "BUILD", build)
+
+	// ---------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "copyright information here",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Print(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+	// ---------------------------------------------------------------------
+	// App Starting
+
+	log.Infow("starting service", "version", build)
+	defer log.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Infow("startup", "config", out)
+
+	// -------------------------------------------------------------------------
+	// Start Debug service
+
+	log.Infow("startup", "status", "debug v1 router started", "host", cfg.Web.DebugHost)
+
+	go func() {
+		if err := http.ListenAndServe(cfg.Web.DebugHost, debug.StandardLibraryMux()); err != nil {
+			log.Errorw("shutdown", "status", "debug v1 router closed", "host", cfg.Web.DebugHost, "ERROR", err)
+		}
+	}()
+
+	// ----------------------------------------------------------------------
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-shutdown
+
+	log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mchengat/service
+
+go 1.20
+
+require go.uber.org/zap v1.25.0
+
+require (
+	github.com/ardanlabs/conf/v3 v3.1.6
+	go.uber.org/multierr v1.10.0 // indirect
+)
